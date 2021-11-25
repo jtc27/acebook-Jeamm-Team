d66db79e1275160e45c13ec2e2342bd38ae1cd50
Jacob Cho <89138654+jtc27@users.noreply.github.com>
2021-11-25 18:11:22 +0000

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     # The welcome index view page renders many other pages (posts, profile etc)
-    # The variables requires from those pages need to be defined here
+    # The variables required from those pages need to be defined here
 
     @posts = Post.order("created_at DESC")
     @post = Post.new
